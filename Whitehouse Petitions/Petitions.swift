24e867b041c733e5c9132003a98199c1fc9093b6
AmirNasser909 <amirnasser989@gmail.com>
2023-08-30 19:16:30 +0300

    - Parsing JSON using the Codable protocol
    - Rendering a petition: loadHTMLString
    
    - Finishing touches: didFinishLaunchingWithOptions

---
+++ Whitehouse Petitions/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Whitehouse Petitions
+//
+//  Created by MacBook Air on 30/08/2023.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
